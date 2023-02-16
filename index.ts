e66204e3d9b249d1916b0f2600fd2917e31030b6
Juan Alejandro Work <alej1286@gmail.com>
2023-02-16 14:46:41 -0500

    Added relation among main clases

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
-
-import express from "express";
+import express from 'express';
 const app = express();
-import db from "./models";
+import db from './models';
 const port = process.env.PORT || 3000;
+import {users} from './seeders/users';
+import {projects} from './seeders/projects'
+import {projectassignments} from './seeders/projectassignments'
+
+app.get('/', (req, res) => {
+    db.User.findAll({
+        include: {
+            model: db.Project
+        }
+    }).then((result: object) => res.json(result)).catch((err: object) => console.error(err));
+})
+
 
 db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`App listtening on port ${port}`);
-  });
-});
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+})
